d8a17ff488ee91cef8b036787aea3e5d3a151ebb
Michiel Dethmers <git@phplist.com>
2014-11-11 17:43:30 +0000

    initial v.0.1

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ epub2html
 =========
 
 Convert ePub to HTML
+
+This simple PHP script will take an ePub file and allow publishing it as a browseable set of pages.
+
+To install:
+
+Assuming the location of the ePub on a website in /manual/
+
+1. Add the following to the Apache VHOST (assuming Mod Rewrite is available)
+
+  RewriteRule ^/manual/(.*).xhtml /manual/index.php/$1 [PT]
+
+2. Download the ePub file in the manual directory and type "unzip file.epub"
+
+Browse to http://www.website.com/manual/
+
+For formatting, update the header.html and footer.html. The current ones are for phpList.
+
+
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * ePub2HTML - quick-n-dirty script to take an ePub and show it as HTML
+ * used for the phpList Manual, edited on flossmanuals.org
+ * 
+ * v.01 - 11/11/2014 - MD initial release
+ * 
+ * license GPLv3 or later
+ * 
+ */
+
+$headerFile = 'header.html';
+$footerFile = 'footer.html';
+$tocFile = 'toc.ncx';
+
+/* ---------------------- end config -------------- */
+
+
+$tocFile = simplexml_load_file(dirname(__FILE__).'/'.$tocFile);
+$title = $tocFile->docTitle->text;
+$toc = extractMenu($tocFile->navMap->navPoint);
+
+$header = file_get_contents(dirname(__FILE__).'/'.$headerFile);
+$footer = file_get_contents(dirname(__FILE__).'/'.$footerFile);
+
+$file = $_SERVER['REQUEST_URI'];
+$file = basename($file);
+if (is_file(dirname(__FILE__).'/'.$file)) {
+  $contents = file_get_contents(dirname(__FILE__).'/'.$file);
+} 
+
+$contents = str_replace('<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><title/>','',$contents);
+
+$header = str_replace('[TITLE]',$title,$header);
+
+print $header;
+print '<table><tr><td valign="top">';
+print $toc;
+print '</td><td valign="top">';
+print $contents;
+print '</td></tr></table>';
+print $footer;
+
+function extractMenu($navPoints,$level = 1) {
+  $menu = '<ul class="menu level'.$level.'">';
+  foreach ($navPoints as $navPoint) {
+    $menu .= '<li class="menuitem itemlevel'.$level.'">';
+    $menu .=  '<a href="';
+    $menu .= $navPoint->content['src'];
+    $menu .= '">';
+    $menu .=  $navPoint->navLabel->text;
+    $menu .= '</a>';
+    
+    if (!empty($navPoint->navPoint)) {
+      $menu .= extractMenu($navPoint->navPoint,$level++);
+    }
+    $menu .= '</li>';
+  }
+  $menu .= '</ul>';
+  return $menu;
+}
+
+
